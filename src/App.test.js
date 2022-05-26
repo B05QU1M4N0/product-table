4a097f75cf3246dc2705a2e1b0deb89c6d15d794
B05QU1M4N0 <marcioclelio22@gmail.com>
2022-05-26 12:00:20 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,125 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { useState } from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly} 
+        onFilterTextChange={setFilterText} 
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable 
+        products={products} 
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
+    </div>
+  );
+}
+
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+  );
+}
+
+function ProductRow({ product }) {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (
+      product.name.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1
+    ) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Modelo</th>
+          <th>Preço</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange
+}) {
+  return (
+    <form>
+      <input 
+        type="text" 
+        value={filterText} placeholder="Pesquisar..." 
+        onChange={(e) => onFilterTextChange(e.target.value)} />
+      <label>
+        <input 
+          type="checkbox" 
+          checked={inStockOnly} 
+          onChange={(e) => onInStockOnlyChange(e.target.checked)} />
+        {' '}
+Mostrar apenas os modelos em estoque.      </label>
+    </form>
+  );
+}
+
+const PRODUCTS = [
+  {category: "kia motors", price:"$20,000", stocked: true, name: "Kia k7"},
+  {category: "kia motors", price: "$16,000", stocked: true, name: "Kia k5"},
+  {category: "kia motors", price: "$17,000", stocked: false, name: "Kia Sportage"},
+  {category: "Hyundai M. Company", price: "$16,500", stocked: true, name: "Hyundai Contineltal"},
+  {category: "Hyundai M. Company", price: "$12,000", stocked: false, name: "Hyundai Azera"},
+  {category: "Hyundai M. Company", price: "$10,000", stocked: true, name: "Hyunday Sonata"}
+];
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
+}
